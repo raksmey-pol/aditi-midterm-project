f6c41da77ee2d02623945f5204a9dbe2424dafb7
Vibol Heng <vibolheng425@gmail.com>
2026-02-17 20:26:47 +0700

    get product home page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-import Container from "@/components/Container";
-import HomeBanner from "@/components/HomeBanner";
-import ProductList from "@/components/ProductList";
+
+import PublicPage from "./(public)/page";
 
 export default function HomePage() {
   return (
-    <div className="space-y-12 pb-16">
-      <Container>
-        <HomeBanner />
-      </Container>
-
-      <ProductList />
-    </div>
+    <PublicPage/>
   );
 }
